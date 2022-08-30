0b40e1141fa98705e31dd56e27e1268fcc2190cf
johnlombardi389 <lombardij38@gmail.com>
2022-08-29 21:15:17 -0400

    Created Book class in js

---
+++ script.js
@@ -0,0 +1,14 @@
+class Book{
+  constructor(
+    title: 'Book title',
+    author: 'Name',
+    pages: 0,
+    read: false
+  )
+  {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
