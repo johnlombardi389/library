3b33f7875f9c16576c4b35188169f784c8871f09
johnlombardi389 <lombardij38@gmail.com>
2022-08-29 21:21:59 -0400

    Created Library class and add add, remove, get, and check functions

--- script.js
+++ script.js
@@ -4,11 +4,34 @@ class Book{
     author: 'Name',
     pages: 0,
     read: false
-  )
-  {
+  ) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+  }
+}
+
+class Library{
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(newBook) {
+    if(!this.isInLibrary(newBook)) {
+      this.books.push(newBook);
+    }
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title);
+  }
+
+  getBook(title) {
+    return this.books.find((book) => book.title === title);
+  }
+
+  isInLibrary(newBook) {
+    return this.books.some((book) => book.title === newBook.title);
   }
 }
